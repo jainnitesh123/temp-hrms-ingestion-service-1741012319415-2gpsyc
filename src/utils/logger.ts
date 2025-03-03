87d3f71e58c3e3df4ff3806a5f5250fafa5037a2
Nitesh <nitesh.jain@loophealth.com>
2025-03-03 20:02:50 +0530

    Copy src/utils/logger.ts from hrms-ingestion-service

---
+++ src/utils/logger.ts
@@ -0,0 +1,18 @@
+import winston from 'winston';
+
+export const setupLogger = () => {
+  return winston.createLogger({
+    level: 'info',
+    format: winston.format.combine(
+      winston.format.timestamp(),
+      winston.format.json()
+    ),
+    transports: [
+      new winston.transports.Console(),
+      new winston.transports.File({ filename: 'error.log', level: 'error' }),
+      new winston.transports.File({ filename: 'combined.log' })
+    ]
+  });
+};
+
+export const logger = setupLogger();
